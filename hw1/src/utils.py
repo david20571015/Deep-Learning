aed4f672cc5bc93ce39ebc3d2212c44bce8419c2
David Chiu <david20571015@gmail.com>
2022-10-23 23:14:54 +0800

    refactor: utils

---
+++ hw1/src/utils.py
@@ -0,0 +1,24 @@
+from src.model import Model
+from src.losses import Loss
+
+
+def split_batch(data, target, batch_size):
+    for i in range(0, len(data), batch_size):
+        if i + batch_size < len(data):
+            yield data[i:i + batch_size], target[i:i + batch_size]
+        yield data[i:], target[i:]
+
+
+def train_step(data, target, model: Model, loss_fn: Loss, learning_rate):
+    pred = model(data)
+    loss = loss_fn(pred, target)
+    grad = loss_fn.backward()
+    model.backward(grad)
+    model.update(learning_rate=learning_rate)
+    return loss
+
+
+def test_step(data, target, model: Model, loss_fn: Loss):
+    pred = model(data)
+    loss = loss_fn(pred, target)
+    return loss
